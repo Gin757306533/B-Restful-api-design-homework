0ee159234d483285f5553e11e81acf6f9a03f413
Gin757306533 <757306533@qq.com>
2020-11-12 20:48:57 +0800

    feat[xj-1]: add controller and service

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.service;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.dto.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StudentService {
+
+    private Map<String, Student> studentMap;
+
+    public StudentService() {
+        this.studentMap = new HashMap<>();
+    }
+
+    public void createStudent(Student student){
+        this.studentMap.put(student.getId(), student);
+    }
+}
