433be2946dabdcf0b3e55f17d1843669f696d140
Gin757306533 <757306533@qq.com>
2020-11-15 19:42:50 +0800

    feat[xj-1]: get student filter by gender or not

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.thoughtworks.capability.gtb.restfulapidesign.dto.Student;
 import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -21,5 +23,15 @@ public class StudentController {
     @GetMapping("/students/{id}")
     public Student getStudentById(@PathVariable String id){
         return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudentById(@PathVariable String id){
+        studentService.deleteStudentById(id);
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudentListByGenderOrNull(@RequestParam(required = false) String gender){
+        return studentService.getStudentListByGenderOrNull(gender);
     }
 }
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/StudentService.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/StudentService.java
@@ -4,7 +4,10 @@ import com.thoughtworks.capability.gtb.restfulapidesign.dto.Student;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -21,5 +24,19 @@ public class StudentService {
 
     public Student getStudentById(String id){
         return this.studentMap.get(id);
+    }
+
+    public void deleteStudentById(String id){
+        this.studentMap.remove(id);
+    }
+
+    public List<Student> getStudentListByGenderOrNull(String gender) {
+        if(gender == null){
+            return studentMap.values().stream().collect(Collectors.toList());
+        }else{
+            return studentMap.values().stream().filter(student ->
+                    student.getGender().equals(gender))
+                    .collect(Collectors.toList());
+        }
     }
 }
